0705b34fc55246a1959118806ed069bc9196c4ea
KapitanKakao <kapitan.kakao52375@gmail.com>
2021-11-24 12:34:50 +0200

    Fix absent zero in bonusRounded

--- src/utils/Rounder.java
+++ src/utils/Rounder.java
@@ -6,7 +6,7 @@ import java.text.DecimalFormat;
 public class Rounder {
     // Округление значения по шаблону
     public static String roundValue(double value) {
-        DecimalFormat df = new DecimalFormat("#.00");
+        DecimalFormat df = new DecimalFormat("0.00");
         return df.format(value);
     }
 }
